617c259e51378b8d58975798015a46a8c734e366
Raul_LA <rleonalvarez090@gmail.com>
2023-09-12 15:25:55 -0500

    tercer commit

--- Ejm2.java
+++ Ejm2.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Ejm2 {
     public static void main(String[] args) {
+        System.out.println("Modificacion 3");
+        
         int num;
         Scanner sc = new Scanner(System.in);
         num = sc.nextInt();
