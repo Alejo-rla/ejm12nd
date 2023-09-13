73938d88da02c9f2fa356bd2286ebe501e875a64
Raul_LA <rleonalvarez090@gmail.com>
2023-09-12 20:06:18 -0500

    mod 2 al fichero holla

--- holla.java
+++ holla.java
@@ -2,8 +2,7 @@ public class holla {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         int a=1;
-        System.out.println("Modificacion ");
-        System.out.println("Modificacion 2");
+   
         System.out.println("Modificacion 3");
 
         System.out.println("Modificacion 4");
